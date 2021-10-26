cb9d5e7b4055f17c1b723034ae523a3e2854c88b
Hieu <hn@localizedirect.com>
2021-10-27 00:13:56 +0700

    Init project

---
+++ src/main/java/com/emeraldhieu/algorithm/Driver.java
@@ -0,0 +1,18 @@
+package com.emeraldhieu.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        List<String> s = new ArrayList<>();
+
+        Driver driver = new Driver();
+        driver.test();
+    }
+
+    public void test() {
+
+    }
+}
---
+++ src/test/java/com/emeraldhieu/algorithm/DriverTest.java
@@ -0,0 +1,13 @@
+package com.emeraldhieu.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+class DriverTest {
+
+    private final Driver driver = new Driver();
+
+    @Test
+    public void test() {
+        driver.test();
+    }
+}
