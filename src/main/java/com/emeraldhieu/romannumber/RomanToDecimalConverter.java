708db5161ea03112a8cba7d50eacef1e2dc45756
emeraldhieu <emerald.hieu@gmail.com>
2022-07-04 23:56:57 +0700

    Replace list of basic numbers to a map from roman letters to decimal numbers

--- src/main/java/com/emeraldhieu/romannumber/RomanToDecimalConverter.java
+++ src/main/java/com/emeraldhieu/romannumber/RomanToDecimalConverter.java
@@ -2,39 +2,33 @@ package com.emeraldhieu.romannumber;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class RomanToDecimalConverter {
 
+    private static final Map<String, Integer> decimalNumbersByRomanLetter = Map.of(
+        "I", 1,
+        "V", 5,
+        "X", 10,
+        "L", 50,
+        "C", 100,
+        "D", 500,
+        "M", 1000
+    );
+
     public int convert(String romanNumber) {
-
-        /**
-         * I don't use a map because unmodifiable map doesn't maintain order of entries.
-         * Of course, I can use LinkedHashMap to do that, but it's not a good solution.
-         * I'd prefer using a named class {@link BasicNumber}.
-         */
-        List<BasicNumber> list = Arrays.asList(
-            new BasicNumber("I", 1),
-            new BasicNumber("V", 5),
-            new BasicNumber("X", 10),
-            new BasicNumber("L", 50),
-            new BasicNumber("C", 100),
-            new BasicNumber("D", 500),
-            new BasicNumber("M", 1000)
-        );
-
         List<String> letters = Arrays.asList(romanNumber.split(""));
-
         int resultNumber = 0;
-
         String previousLetter = "";
+
         for (int i = 0; i < letters.size(); ++i) {
             String letter = letters.get(i);
-            int number = getNumber(list, letter);
+            int number = decimalNumbersByRomanLetter.get(letter);
 
             resultNumber += number;
 
             if (!previousLetter.isEmpty()) {
-                int previousNumber = getNumber(list, previousLetter);
+                int previousNumber = decimalNumbersByRomanLetter.get(previousLetter);
                 if (previousNumber < number) {
                     resultNumber = resultNumber - previousNumber - previousNumber;
                 }
@@ -44,14 +38,5 @@ public class RomanToDecimalConverter {
         }
 
         return resultNumber;
-    }
-
-    private int getNumber(List<BasicNumber> list, String letter) {
-        int number = list.stream()
-            .filter(basicNumber -> basicNumber.getRomanNumber().equals(letter))
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException("Something wrong"))
-            .getDecimalNumber();
-        return number;
     }
 }
